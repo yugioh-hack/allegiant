729034b4ae2503ab0fef0ae5fdd010463c7c4e3f
yugioh-hack <yugiohhack.enl@gmail.com>
2018-09-24 23:21:09 +0900

    サムネイルを記事の中にいれ、画像を小さくした

--- index.php
+++ index.php
@@ -4,25 +4,25 @@
 
 <?php if ( cpotheme_show_posts() ) : ?>
 <div id="main" class="main">
-	<div class="container">		
-		<section id="content" class="content">
-			<?php do_action( 'cpotheme_before_content' ); ?>
-			<?php
-			if ( have_posts() ) {
-				while ( have_posts() ) :
-					the_post();
-				?>
-							<?php get_template_part( 'template-parts/element', 'blog' ); ?>
-			<?php
-			endwhile;
-			};
+  <div class="container">
+    <section id="content" class="content">
+      <?php do_action( 'cpotheme_before_content' ); ?>
+      <?php
+      if ( have_posts() ) {
+        while ( have_posts() ) :
+          the_post();
+        ?>
+              <?php get_template_part( 'template-parts/element', 'thumbnailblog' ); ?>
+      <?php
+      endwhile;
+      };
 ?>
-			<?php cpotheme_numbered_pagination(); ?>
-			<?php do_action( 'cpotheme_after_content' ); ?>
-		</section>
-		<?php get_sidebar(); ?>
-		<div class="clear"></div>
-	</div>
+      <?php cpotheme_numbered_pagination(); ?>
+      <?php do_action( 'cpotheme_after_content' ); ?>
+    </section>
+    <?php get_sidebar(); ?>
+    <div class="clear"></div>
+  </div>
 </div>
 <?php endif; ?>
 
---
+++ template-parts/element-thumbnailblog.php
@@ -0,0 +1,25 @@
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+  <div class="post-body">
+    <?php y_cpotheme_postpage_image(); ?>
+
+    <div class="post-body__mainContent">
+      <?php cpotheme_postpage_title(); ?>
+      <div class="post-byline">
+        <?php cpotheme_postpage_date(); ?>
+        <?php cpotheme_postpage_author(); ?>
+        <?php cpotheme_postpage_categories(); ?>
+        <?php cpotheme_edit(); ?>
+      </div>
+      <div class="post-content">
+        <?php cpotheme_postpage_content(); ?>
+      </div>
+      <?php cpotheme_postpage_comments( false, '%s' ); ?>
+      <?php
+      if ( is_singular( 'post' ) ) {
+        cpotheme_postpage_tags( false, '', '', '' );}
+  ?>
+      <?php cpotheme_postpage_readmore( 'button' ); ?>
+      <div class="clear"></div>
+    </div>
+  </div>
+</article>
